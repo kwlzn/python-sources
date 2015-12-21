34f02cb50ea2b192ca14f080e36415819b21265b
Kris Wilson <kwilson@twopensource.com>
2015-12-21 14:39:55 -0800

    Add update script.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+
 # python-sources
-a "one-stop shop" reference for python stdlib source code.
+
+ever wanted to send a quick link to specific line numbers in the python stdlib's source code as referenced by that pesky traceback?
+
+now you can! python-sources is a collection of versioned python source code from 2.6.x -> 3.5.x that is made available on github for linking and quick reference.
---
+++ update.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+TRACKED_VERSIONS="
+2.6
+2.6.1
+2.6.2
+2.6.3
+2.6.4
+2.6.5
+2.6.6
+2.6.7
+2.6.8
+2.6.9
+2.7
+2.7.1
+2.7.10
+2.7.11
+2.7.2
+2.7.3
+2.7.4
+2.7.5
+2.7.6
+2.7.7
+2.7.8
+2.7.9
+3.0
+3.0.1
+3.1
+3.1.1
+3.1.2
+3.1.3
+3.1.4
+3.1.5
+3.2
+3.2.1
+3.2.2
+3.2.3
+3.2.4
+3.2.5
+3.2.6
+3.3.0
+3.3.1
+3.3.2
+3.3.3
+3.3.4
+3.3.5
+3.3.6
+3.4.0
+3.4.1
+3.4.2
+3.4.3
+3.4.4
+3.5.0
+3.5.1
+"
+
+for VER in $TRACKED_VERSIONS; do
+  PY_VER="Python-${VER}"
+  PY_TAR="${PY_VER}.tgz"
+  if [ ! -e $PY_VER ]; then
+    wget -q --show-progress https://www.python.org/ftp/python/${VER}/${PY_TAR} && \
+    tar zxf ${PY_TAR} && \
+    rm -rf ${PY_TAR}
+  else
+    echo "skipping existing ${PY_VER}"
+  fi
+done
