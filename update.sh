babb90b39aa4be22741084770e00fd12111c5383
Kris Wilson <kwilson@twopensource.com>
2021-07-12 00:27:48 -0700

    Update sources.

--- update.sh
+++ update.sh
@@ -51,13 +51,54 @@ TRACKED_VERSIONS="
 3.5.2
 3.5.3
 3.6.0
+3.6.1
+3.6.2
+3.6.3
+3.6.4
+3.6.5
+3.6.6
+3.6.7
+3.6.8
+3.6.9
+3.6.10
+3.6.11
+3.6.12
+3.6.13
+3.7.0
+3.7.1
+3.7.2
+3.7.3
+3.7.4
+3.7.5
+3.7.6
+3.7.7
+3.7.8
+3.7.9
+3.7.10
+3.8.0
+3.8.1
+3.8.2
+3.8.3
+3.8.4
+3.8.5
+3.8.6
+3.8.7
+3.8.8
+3.8.9
+3.8.10
+3.9.0
+3.9.1
+3.9.2
+3.9.3
+3.9.4
+3.9.5
 "
 
 for VER in $TRACKED_VERSIONS; do
   PY_VER="Python-${VER}"
   PY_TAR="${PY_VER}.tgz"
   if [ ! -e $PY_VER ]; then
-    wget -q --show-progress https://www.python.org/ftp/python/${VER}/${PY_TAR} && \
+    curl -s#O https://www.python.org/ftp/python/${VER}/${PY_TAR} && \
     tar zxf ${PY_TAR} && \
     rm -rf ${PY_TAR}
   else
